b6002387f924afb5ddeaa1ce9fa96ac93644d8b7
Paul Iakushenkov <yakushenkovpaul@gmail.com>
2019-01-25 15:52:36 +0300

    dada

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 
-echo 'aaaa';
-exit;
-
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
